29559ac8a910b00515642fdb8fefaa6648b49e65
Eber27 <eberfreire@gmail.com>
2021-05-14 13:56:36 -0500

    ChallengeSet4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,122 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    result += num;
+                }
+                else
+                {
+                    result -= num;
+                }
+            }
+
+            return result;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int shortLength = str1.Length;
+            var stringList = new List<string> { str1, str2, str3, str4 };
+
+            foreach (var str in stringList)
+            {
+                if (str.Length < shortLength)
+                {
+                    shortLength = str.Length;
+                }
+            }
+
+            return shortLength;
         }
+
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var numbersList = new List<int> { number1, number2, number3, number4 };
+            numbersList.Sort();
+
+            return numbersList.ElementAt(0);
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return double.TryParse(input, out _);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int isNull = 0;
+            int notNull = 0;
+            foreach (var objects in objs)
+            {
+                if (objects == null)
+                {
+                    isNull++;
+                }
+                else
+                {
+                    notNull++;
+                }
+            }
+            return isNull > notNull;
         }
+
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            double numTotal = 0;
+            double count = 0;
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    numTotal += num;
+                    count++;
+                }
+            }
+            if (numTotal == 0)
+            {
+                return numTotal;
+            }
+            return numTotal / count;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number == 0)
+            {
+                return 1;
+            }
+            var result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+            return result;
         }
     }
 }
