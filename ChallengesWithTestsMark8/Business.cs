af3228f3da84da91b7ac1a0529d5d06bbfa8874c
Eber27 <eberfreire@gmail.com>
2021-06-04 16:01:07 -0500

    SET_5

--- ChallengesWithTestsMark8/Business.cs
+++ ChallengesWithTestsMark8/Business.cs
@@ -3,6 +3,7 @@ namespace ChallengesWithTestsMark8
 {
     public class Business
     {
+        public static int Length { get; internal set; }
         public string Name { get; set; }
         public double TotalRevenue { get; set; }
         public double TotalExpenses { get; set; }
